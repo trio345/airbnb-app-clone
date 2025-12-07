9142447e8a927d5a874ec4cb60681bb71591e146
Trio Saputra <triosaputra308@gmail.com>
2025-12-07 22:06:05 +0700

    add proxy

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from 'next/server'
 export { default } from 'next-auth/middleware';
 
 export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.redirect(new URL('/', request.url))
 }
 
 export const config = {
